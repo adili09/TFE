29f30240f1f9feaaa6ae357674539832678c6b6c
adili09 <101093541+adili09@users.noreply.github.com>
2023-01-18 17:04:08 +0100

    animation_journey
    animation modal manquante

---
+++ script.js
@@ -0,0 +1,5 @@
+const sidebarBtnToggle = document.getElementById("sidebarBtnToggle");
+
+
+const modalBtn = document.getElementById("modalBtn");
+modalBtn.addEventListener('click', () => { console.log('coucou') })
